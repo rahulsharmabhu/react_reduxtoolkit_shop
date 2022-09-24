c1b5c9bf437b6cd253fa9f7088ee193c3e79b7a6
Rahul Sharma <rahulsharmabhumca16@gmail.com>
2022-09-24 09:29:09 +0530

    added the thunk and final functionality....|| completed

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,37 @@
-import React, { useState,useEffect } from 'react'
-import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react'
+import { useDispatch,useSelector } from 'react-redux';
 import { add } from '../store/cartSlice';
+import { fetchProducts } from '../store/productsSlice';
+import { STATUSES } from '../store/productsSlice';
+
 const Products = () => {
-  const [products,setProducts] = useState([]);
+
+  //const [products,setProducts] = useState([]);
   const dispatch = useDispatch();
+  const {data:products,status} = useSelector((state) => state.product)
+
   useEffect(() => {
-  const fetchProduct = async () => {
-    const res = await fetch('https://fakestoreapi.com/products/');
-    const data = await res.json();
-    console.log(data);
-    setProducts(data);
-  }
-  fetchProduct();
+
+   dispatch(fetchProducts());
+  // const fetchProduct = async () => {
+  //   const res = await fetch('https://fakestoreapi.com/products/');
+  //   const data = await res.json();
+  //   console.log(data);
+  //   setProducts(data);
+  // }
+  // fetchProduct();
   }, []);
 
   const handleAdd = (product) => {
     dispatch(add(product));
+  };
+
+  if(status === STATUSES.LOADING){
+    return <h2 style={{textAlign:'center'}}>Loading...</h2>
+  }
+
+  if(status === STATUSES.ERROR){
+    return <h2 style={{textAlign:'center'}}>Something went wrong!</h2>
   }
   
 
